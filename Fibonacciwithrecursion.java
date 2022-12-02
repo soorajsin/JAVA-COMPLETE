e859a9215b8c2d22a81936e2cad36033274ab035
soorajsin <115391013+soorajsin@users.noreply.github.com>
2022-12-02 22:21:17 +0530

    Add files via upload

---
+++ Fibonacciwithrecursion.java
@@ -0,0 +1,19 @@
+public class Fibonacciwithrecursion {
+          static int n1=0,n2=1,n3=0;
+          static void print_fibonacci(int count){
+                    if(count>0){
+                              n3=n1+n2;
+                              n1=n2;
+                              n2=n3;
+                              System.out.print("  "+n3);
+                              print_fibonacci(count-1);
+                    }
+          }
+          public static void main(String[] args) {
+                    int count=20;
+                    System.out.print(n1+" "+n2);
+                    
+                    print_fibonacci(count);
+                    
+          }
+}
