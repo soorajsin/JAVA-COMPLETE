01ed1b2c3810a0e897e50c046d6b06d17a1ac668
soorajsin <115391013+soorajsin@users.noreply.github.com>
2022-12-03 22:36:05 +0530

    Add files via upload

---
+++ LeftShiftOperator.java
@@ -0,0 +1,17 @@
+public class LeftShiftOperator {
+        public static void main(String args[]) 
+        {     
+          // int a=8;//8=1000
+          // int b;
+          // b=a>>2;
+         // System.out.println(b);   
+             byte x = 64;
+            System.out.println(x<<2);
+             int i;
+             byte y; 
+             i = x << 2;
+             y = (byte)(x <<2);
+             System.out.print(i + " " + y);
+             
+        } 
+    }
